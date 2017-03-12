31b06cb5ebc5cd54aa88fad45a38bac04802a4c0
IgorKnyish <lukasskott100@gmail.com>
2017-03-12 12:32:55 +0000

    Init

--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
 
-//clock
-
-
-//scroll up
 $(function() {
     $(window).scroll(function() {
         if($(this).scrollTop() != 0) {
@@ -13,14 +9,33 @@ $(function() {
     $('#toTop').click(function() {
         $('body,html').animate({scrollTop:0},1500);
     });
+  if($(window).width() <= 766 ){
+ $( "#nav" ).removeClass( "navbar navbar-inverse" ).addClass( "navbar navbar-inverse navbar-default navbar-fixed-bottom" );
+} 
+if($(window).width() > 766 ){
+ $( "#nav" ).removeClass( "navbar navbar-inverse  navbar-default navbar-fixed-bottom" ).addClass( "navbar navbar-inverse" );
+} 
+
+function resizeScreen(){ 
+if($(window).width() <= 766 ){
+ $( "#nav" ).removeClass( "navbar navbar-inverse" ).addClass( "navbar navbar-inverse navbar-default navbar-fixed-bottom" );
+} 
+if($(window).width() > 766 ){
+ $( "#nav" ).removeClass( "navbar navbar-inverse  navbar-default navbar-fixed-bottom" ).addClass( "navbar navbar-inverse" );
+} 
+} 
+$(window).resize(function() { 
+resizeScreen(); 
+}); 
      new WOW().init();
      setInterval(function() {
 	target.innerHTML = (new Date()).toLocaleTimeString();
 }, 1000);
 });
 
+
 //statistics
-document.querySelector('#stats').addEventListener('click', getStat);
+$("#stats").bind("click", getStat);
 function getStat() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
